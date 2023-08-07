67d28bc4f94970ac227b7e56f200e416081e43f5
manu <manuxcamargo.564@gmail.com>
2023-08-07 14:21:08 -0300

    show post content in html

--- js/script.js
+++ js/script.js
@@ -6,16 +6,43 @@ function savePost() {
     const publisher = document.getElementById("publisher").value;
     const date = document.getElementById("date").value;
 
-    if(title && resume && publisher && date){
+    console.log(title, resume, publisher, date)
+
+    if (title && resume && publisher && date) {
         storePost(title, resume, publisher, date)
     }
 }
 
-function storePost (title, resume, publisher, date) {
+function storePost(title, resume, publisher, date) {
     const post = {
-        title: title,
-        resume: resume,
-        publisher: publisher,
-        date: date
-    }
+        title,
+        resume,
+        publisher,
+        date
+    };
+
+    posts.push(post);
+    showPosts();
+
+   
+}
+
+function showPosts() {
+    let showContent = "";
+
+    posts.forEach((post, index) => {
+        showContent += `
+        <div class="post">
+        <h2>${post.title}</h2>
+        <p><strong>Resumo:</strong>${post.resume}></p>
+        <p><strong>Autor:</strong>${post.publisher}></p>
+        <p><strong>Data de publicação:</strong>${post.date}></p>
+
+        <button onclick="editPost(${index})">Editar</button>
+        <button onclick="removePost(${index}">Remover</button>
+        </div>
+        `;
+    })
+
+    document.getElementById("list").innerHTML = showContent;
 }
