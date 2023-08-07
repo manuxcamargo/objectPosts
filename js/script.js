01f164138c4a0c411cccf10d005a0f27cc4ba98a
manu <manuxcamargo.564@gmail.com>
2023-08-07 11:18:39 -0300

    script part1

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const posts = [];
+
+function savePost() {
+    const title = document.getElementById("title").value;
+    const resume = document.getElementById("resume").value;
+    const publisher = document.getElementById("publisher").value;
+    const date = document.getElementById("date").value;
+
+    if(title && resume && publisher && date){
+        storePost(title, resume, publisher, date)
+    }
+}
+
+function storePost (title, resume, publisher, date) {
+    const post = {
+        title: title,
+        resume: resume,
+        publisher: publisher,
+        date: date
+    }
+}
